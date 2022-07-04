42f581d6fce61a7e13f8a9b04e2d34c2180a8f57
mukerdev <rawgmuker@naver.com>
2022-07-04 14:05:31 +0900

    commit

---
+++ 3.Diary/Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by Muker on 2022/07/04.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ 3.Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by Muker on 2022/07/04.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
